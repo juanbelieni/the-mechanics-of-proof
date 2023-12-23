eb773873475f723d6c3fcf4cf3b4be86c9d47262
Juan Belieni <juanbelieni@gmail.com>
2023-12-23 15:57:34 -0300

    Complete section 1

--- Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
+++ Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
@@ -6,7 +6,8 @@ attribute [-instance] Int.instDivInt_1 Int.instDivInt Nat.instDivNat
 
 /-! # Section 1.5: A shortcut -/
 
-example {x : ℤ} (h1 : x + 4 = 2) : x = -2 := by addarith [h1]
+example {x : ℤ} (h1 : x + 4 = 2) : x = -2 := by
+  addarith [h1]
 
 example {a b : ℤ} (ha : a - 2 * b = 1) : a = 2 * b + 1 := by addarith [ha]
 
@@ -22,4 +23,9 @@ example {m n : ℝ} (h1 : m ≤ 8 - n) : 10 > m + n := by addarith [h1]
 
 
 -- Check that `addarith` can't verify this deduction!
-example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 := sorry
+example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 := by
+  -- addarith [h1]
+  calc
+    w = (3 * w + 1) / 3 - 1 / 3 := by ring
+    _ = 4 / 3 - 1 / 3:= by rw [h1]
+    _ = 1 := by ring
